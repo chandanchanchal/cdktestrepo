394754d384d758e6de9d21657394e118c0d3005e
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2023-07-31 16:03:19 +0530

    Create CdkStarterStack.ts

---
+++ lib/CdkStarterStack.ts
@@ -0,0 +1,18 @@
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cdk from 'aws-cdk-lib';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // import existing IAM Role
+    const importedRole = iam.Role.fromRoleArn(
+      this,
+      'imported-role',
+      `arn:aws:iam::${cdk.Stack.of(this).account}:role/Existing-Role-Name`,
+      {mutable: false},
+    );
+
+    console.log('importedRole ', importedRole.roleName);
+  }
+}
